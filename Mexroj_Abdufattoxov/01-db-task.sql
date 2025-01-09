b7af26d31a0871de2b9ed750ce0397a73939cc9d
Mexroj02 <mabdufattoxov02@gmail.com>
2025-01-09 11:07:44 +0500

    Update 01-db-task.sql

--- Mexroj_Abdufattoxov/01-db-task.sql
+++ Mexroj_Abdufattoxov/01-db-task.sql
@@ -1,16 +1,16 @@
-CREATE TABLE students (
+CREATE TABLE IF NOT EXISTS students (
     student_id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     birth_date DATE,
     age INT,
     enrollment_year INT);
 
-CREATE TABLE courses (
+CREATE TABLE IF NOT EXISTS courses (
     course_id SERIAL PRIMARY KEY,
     course_name VARCHAR(100),
     credit_hours INTEGER);
 
-CREATE TABLE enrollments (
+CREATE TABLE IF NOT EXISTS enrollments (
     enrollment_id SERIAL PRIMARY KEY,
     student_id INTEGER REFERENCES students(student_id),
     course_id INTEGER REFERENCES courses(course_id),
